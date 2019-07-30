ef8c4c798b6b8c92ed6c619c595b1f0070b09d5f
Lukas Jocham <45790416+lukasjoc@users.noreply.github.com>
2019-07-30 12:28:57 +0200

    Update brewCasks.sh

--- brewCasks.sh
+++ brewCasks.sh
@@ -1,2 +1,2 @@
-brew cask install alfred docker iterm2 postman vlc evernote go2shell java spotify whatsapp slack
+brew cask install alfred docker iterm2 postman vlc evernote go2shell spotify whatsapp slack
 cleanmymac figma google-chrome coconutbattery firefox insomnia visual-studio-code
