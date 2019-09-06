c37b05c96d9949a3882188cd3b1560de5cd98d89
lukasjoc <l@lukasjoc.com>
2019-09-06 22:00:49 +0200

    add fight docker script

---
+++ scripts/fightDocker.sh
@@ -0,0 +1,15 @@
+#!/bin/sh 
+
+echo "I'm gonna go to battle for you. I'l triumph or die in honor."
+
+echo "\nFighting containers.."
+docker container stop $(docker container ls -a -q)
+
+echo "\nFighting imges.."
+docker rmi $(docker images -q)
+
+echo "\nFighting networks and docker junk.."
+docker system prune
+
+echo "\aI did it sir $USER. I won with honor"
+
