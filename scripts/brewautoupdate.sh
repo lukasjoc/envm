a23e09555e97d5a465d874508eaf19c5a1590191
lukasjoc <jochamlu@gmail.com>
2019-10-01 15:47:09 +0200

    add figlet and more echos

--- scripts/brewautoupdate.sh
+++ scripts/brewautoupdate.sh
@@ -1,17 +1,29 @@
 #!/bin/sh
 
-function main() {
-  echo "Updating and Upgrading Homebrew/Core"
-  cd /usr/local/Homebrew/Library/Taps/homebrew/homebrew-core
-  git reset --hard origin/master
-  brew upgrade
+cat << "EOF"
+ _                                  _                        _       _
+| |__  _ __ _____      ____ _ _   _| |_ ___  _   _ _ __   __| | __ _| |_ ___
+| '_ \| '__/ _ \ \ /\ / / _` | | | | __/ _ \| | | | '_ \ / _` |/ _` | __/ _ \
+| |_) | | |  __/\ V  V / (_| | |_| | || (_) | |_| | |_) | (_| | (_| | ||  __/
+|_.__/|_|  \___| \_/\_/ \__,_|\__,_|\__\___/ \__,_| .__/ \__,_|\__,_|\__\___|
+                                                  |_|
+EOF
 
-  echo "Upgrading casks if any..."
-  brew cask upgrade
+<< EOF
+lukasjoc, 2019
+https://lukasjoc.com
+===================================================
+EOF
 
-  echo "Cleaning up..."
-  brew cleanup
-}
+echo "Updating and Upgrading Homebrew/Core"
+cd /usr/local/Homebrew/Library/Taps/homebrew/homebrew-core
+git reset --hard origin/master
+brew upgrade
 
-main
+echo "Upgrading casks if any..."
+brew cask upgrade
 
+echo "Cleaning up..."
+brew cleanup
+
+echo "Homebrew and installables up-to-date, $USER"
