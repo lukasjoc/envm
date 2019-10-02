5b257eee1c190af01176d8635dc893bf0bfab859
Lukas Jocham <lukas.jocham@jochen-schweizer.de>
2019-10-02 12:33:31 +0200

    add brewer script fix atomic brewer

--- scripts/homebrew/atomicBrewer.sh
+++ scripts/homebrew/atomicBrewer.sh
@@ -7,28 +7,17 @@ https://lukasjoc.com
 ===================================================
 EOM
 
-# updatebrew is updating the local homebrew-core repositoy
-# and upgrading brew's if possible
-function brew() {
-  echo "Updating and Upgrading Homebrew/Core"
+function update() {
+	echo "Updating and Upgrading Homebrew/Core"
 	cd /usr/local/Homebrew/Library/Taps/homebrew/homebrew-core
 	git reset --hard origin/master
-	brew upgrade
-}
+	brew upgrade	
 
-# updatebrewcask is updating and upgrading cask's if possible
-function cask() {
 	echo "Upgrading casks if any..."
 	brew cask upgrade
-}
 
-# homebrewcleanup is cleaning the homebrew installation if possible
-function cleanup() {
 	echo "Cleaning up..."
 	brew cleanup
 }
 
-#TODO: This is not working right now, executing to infinity
-brew
-cask
-cleanup
+update
---
+++ scripts/homebrew/brewer.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+
+cat << "EOM"
+ _                                
+| |__  _ __ _____      _____ _ __ 
+| '_ \| '__/ _ \ \ /\ / / _ \ '__|
+| |_) | | |  __/\ V  V /  __/ |   
+|_.__/|_|  \___| \_/\_/ \___|_|                                  
+EOM
+
+cat << "EOM"
+lukasjoc, 2019
+https://lukasjoc.com
+Installs Homebrew and brew caskroom
+Gives you some profiles of software to install
+- Default Pack
+- Developer Pack
+===================================================
+EOM
+
+function installbrew() {
+  echo "Installing Homebrew on $USER's mac"
+}
+
+installbrew
