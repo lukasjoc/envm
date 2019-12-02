f9b7f54ae113d1c7dc7cb8db85f44d88379a8792
LUkas Jocham <jochamlu@gmail.com>
2019-12-02 23:22:19 +0100

    Finish Automatic Update

--- envm.sh
+++ envm.sh
@@ -1,21 +1,37 @@
-# ENVM Initialization...
-
 for script ($ENVM/scripts/*sh); do
   source $script
 done
 
-if [[ $ENVM_WDIR == "" ]]; then
+if [ ! -v ENVM_WDIR ]; then
+  echo "Wdir not set"
   export ENVM_WDIR="$HOME/Sync/"
 fi
 
-if [[ $ENABLE_ENVM_AUTO_UPDATE == "true" ]]; then
-  if [[ ! $(git status -uno) ]]; then
-    echo "Updating envm..."
-    CURR_DATE="$(sdate)"
-    REV_HEAD="$(git rev-pase HEAD)"
-    NEWDIR="/tmp/envm-HEAD-$REV_HEAD-$CURR_DATE"
-    touch $NEWDIR
-    echo "Processing pull... Writing logs into $NEWDIR"
-    git pull | 2>&1 | tee $NEWDIR
+if [ ! -d "$ENVM_WDIR" ]; then
+  echo "Wdir does not exist"
+fi
+
+# ------------------------------------------------
+echo "Hello, $USER. Happy Coding.. :)"
+# ------------------------------------------------
+
+if [ -v ENABLE_ENVM_AUTO_UPDATE ] || [[ $ENABLE_ENVM_AUTO_UPDATE == true ]]; then
+  DAT_FILE="$ENVM/cache/start_epoch.dat"
+  if [ ! -f $DAT_FILE ]; then
+    touch $DAT_FILE
+    printf "$(date +%s)" > $DAT_FILE
+    value=`cat $DAT_FILE`
+  else
+    value=`cat $DAT_FILE`
+  fi
+
+  declare -i UPDATE_EPOCH=$(( 3600 * $ENVM_AUTO_UPDATE_DAYS + ${value} ))
+  if [[ $(date +%s) -ge $UPDATE_EPOCH ]]; then
+    echo "Updating..."
+    cd $ENVM
+    git ch master
+    git pull
+    cd $ENVM_WDIR
+    printf "$(date +%s)" > $DAT_FILE
   fi
 fi
