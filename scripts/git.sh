b6cb51c04873ae1492e89f410a700011e67a89cb
Lukas Jocham <jochamlu@gmail.com>
2019-12-16 20:56:57 +0100

    Update Remote Brach Command In Git Branch State Script

--- scripts/git.sh
+++ scripts/git.sh
@@ -3,6 +3,7 @@ LOCAL=$(git rev-parse @)
 REMOTE=$(git rev-parse "$UPSTREAM")
 BASE=$(git merge-base @ "$UPSTREAM")
 
+git remote update
 checkBr() {
   if [ $LOCAL = $REMOTE ]; then
     echo "Up-to-date"
