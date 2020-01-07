57e7e7a425293f6af1386db647fa7172df3c0cb1
Lukas Jocham <lukas.jocham@jochen-schweizer.de>
2020-01-07 12:44:48 +0100

    update buildsh script

--- stuff/build.sh
+++ stuff/build.sh
@@ -1,14 +1,14 @@
 #!/bin/sh
 
 # Builds golang projects for  Mac(default) and Linux with supplied arg $1=linux
-
+# args: $1==system, $2==binary_name
 if [ $(command -v "go") ]; then
   if [ "$1" == "linux" ]; then
     echo "Building for linux amd64"
-    GOOS=linux GOARCH=amd64 go build -o {{EXECUTABLE_NAME}}
+    GOOS=linux GOARCH=amd64 go build -o $2
   else
     echo "Building for darwin 386"
-    GOOS=darwin go build -o {{EXECUTABLE_NAME}}
+    GOOS=darwin GOARCH=amd64 go build -o $2
   fi
 fi
 
