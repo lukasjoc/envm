b64accc4d06435c2e8f82f562a4ab9580cdee992
Lukas Jocham <lukas.jocham@jochen-schweizer.de>
2020-03-18 09:46:41 +0100

    make script a bit more robust, add fortune command for happy random quotes

--- init.sh
+++ init.sh
@@ -5,31 +5,36 @@ for script in $envm/scripts/*.sh; do
 done
 
 if [ -z ${envm_wdir+x} ]; then
-  echo "wdir not set"
+  echo "could not find envm_wdir environment variable"
 	echo "Please set envm_wdir variable like this echo 'export envm_wdir='~/some/dir'' >> ~/.bashrc "
 fi
 
 if [ ! -z ${envm_wdir+x} ]; then
 	if [ ! -d "$envm_wdir" ]; then
-  	echo "wdir does not exist"
+  	echo "could not find working directory as requested in envm_wdir"
 	fi
 fi
 
 # Msg ------------------------------------------------
 figlet "Hello, $USER"
-echo "Happy Coding... :)"
+fortune
 # ------------------------------------------------
 
 # automatic update looking for the value in ./cache/start_epoch.dat
 if [[ $envm_auto_update_days -ge 1 ]]; then
-  dat_file="$envm/cache/start_epoch.dat"
-  if [ ! -f $dat_file ]; then
-    date +%s  > $dat_file
-  fi
+	if [ ! -z ${envm+x} ]; then
+		dat_file="$envm/cache/start_epoch.dat"
+  	if [ ! -f $dat_file ]; then
+   		date +%s  > $dat_file
+  	fi
 
-  declare -i update_epoch=$(( 60 * 60 * 24 * $envm_auto_update_days + $(cat $dat_file) ))
-  if [[ $(date +%s) -ge $update_epoch ]]; then
-    date +%s > $dat_file
-    envm --update
-  fi
+  	declare -i update_epoch=$(( 60 * 60 * 24 * $envm_auto_update_days + $(cat $dat_file) ))
+  	if [[ $(date +%s) -ge $update_epoch ]]; then
+    	date +%s > $dat_file
+    	envm --update
+  	fi
+	else
+		echo "could not find envm source directory"
+	fi
 fi
+
